3b39059d143cde042fd52b5dd10d3cacfb2136d3
职志杰 <786130168@qq.com>
2021-11-07 21:42:04 +0800

    dev

---
+++ demo/src/main/java/com/example/demo/hello/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.demo.hello;
+
+/**
+ * @Author zj
+ * @Date 2021/11/7 20:57
+ * @Description
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World! From dev");
+    }
+}
